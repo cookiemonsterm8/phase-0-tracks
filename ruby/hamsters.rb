0ccadc4938772a9210dbb0e1ca92d1d4eee790fa
kmbradshaw <kaitlynmbradshaw@gmail.com>
2016-05-24 17:15:16 -0500

    added value types and display user inputs

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,17 +2,35 @@
 puts "What is the hamster's name?"
 hamname = gets.chomp
 #Hamster's volume
-puts "How loud is your hamster?"
-volume = gets.chomp
+puts "How loud is your hamster (1 to 10)?"
+volume = gets.to_i
 #furcolor
 puts "What color is your hamster?"
 fur = gets.chomp
 #adoptability
-puts "Is your hamster a good candidate for adoption?"
+puts "Is your hamster a good candidate for adoption (yes or no)?"
 adoptability = gets.chomp
 #age
 puts "How old is your hamster?"
 age = gets.chomp
-if age == ""
+if age.empty?
 	age = nil
+else 
+	age = age.to_f
+end
+
+puts "You are all set."
+puts "Hamster name: #{hamname}"
+puts "Volume: #{volume}"
+puts "Color: #{fur}"
+if adoptability == "yes"
+	puts "Your hamster is a good candidate for adoption!"
+else 
+	puts "Your hamster is not a good candidate for adoption :/"
+end
+
+if age == nil
+	puts "Hamster age not known"
+elsif
+	puts "Your hamster is #{age}"
 end
