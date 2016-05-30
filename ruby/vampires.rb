e116f83304be15820570959be256d6afb2085148
Milan Cook <milancook@gmail.com>
2016-05-30 06:09:13 -0500

    added initial questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+#asks for name
+puts "What is your name?"
+#asks for age
+puts "How old are you?"
+#asks for birthplace
+puts "Where were you born?"
+#asks about garlic
+puts "Our company serves garlic bread. Shall we order some for you?"
+#immortality question
+puts "Would you like to take advantage of our health insurance program?"
