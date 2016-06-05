bb787b0ab2ccd2ecef5c89385e3634490ab21045
Milan Cook <milancook@gmail.com>
2016-06-05 17:23:46 -0500

    switched controls in pair

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,37 @@ def prints_a_thing
 end    
 
 prints_a_thing {|learning1, learning2| puts "Specifically, we are learning #{learning1} and #{learning2}"}
+
+animals = ["cat", "dogs", "snakes", "mice"]
+loud_animals = []
+p animals
+
+animals.each do |animal|
+loud_animals << animal.upcase
+end 
+p loud_animals
+
+p animals
+
+animals.map! do |upcase_animals|
+p upcase_animals.upcase
+end
+
+p animals
+
+
+our_pets = {
+    :black_cat => "Mewpers", 
+    :tabby_cat => "Crookshanks", 
+    :yorkie => "Thomas", 
+    :maltipoo => "Arwen"
+}
+
+p our_pets
+
+our_pets.each do |type, name|
+    puts "#{name} is a #{type}"
+    
+end
+
+p our_pets
