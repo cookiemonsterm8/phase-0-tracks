ecdcf702afb74d9cc391a4c72affe2355a9e50a4
Milan Cook <milancook@gmail.com>
2016-06-07 18:52:30 -0500

    Added pair's work

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -25,20 +25,63 @@ class Puppy
 		puts "Initializing new puppy instance.."
 end
 
-puppy = Puppy.new
-
-puppy.fetch "ball"
-
-puppy.speak 3
-
-puppy.roll_over
-
-puts puppy.dog_years 9
-
-puppy.play_dead
-
-
-
-
-
-
+class Robot
+ 
+   def initialize
+     puts "Initializing Robot"
+   end
+ 
+   def search_for_Luke_Skywalker
+     puts "Couldn't find Luke. Bleep Boop."
+   end
+ 
+   def walk(number_of_steps)
+     number_of_steps.times { puts "Clomp." }
+   end
+ 
+   def calculate_distance(distance)
+     if distance > 2
+       puts "Too far; I'm lazy."
+     else
+       puts "Ok I'll walk #{distance} steps."
+       walk(distance)
+     end
+     distance
+   end
+ end
+ 
+ # puppy = Puppy.new
+ # puppy2 = Puppy.new
+ 
+ # puppy.fetch "ball"
+ 
+ # puppy.speak 3
+ 
+ # puppy.roll_over
+ 
+ # puts puppy.dog_years 9
+ 
+ # puppy.play_dead
+ 
+ # robot_instance = Robot.new
+ # robot_instance.search_for_Luke_Skywalker
+ # robot_instance.walk(10)
+ # robot_instance.calculate_distance(5)
+ # robot_instance.calculate_distance(1)
+ 
+ robot_army = []
+ ctr = 0
+ 
+ while ctr < 50
+   robot_army << Robot.new
+   ctr += 1
+ end
+ 
+ puts robot_army.length
+ 
+   robot_army.each_with_index do |robot, index|
+   robot.search_for_Luke_Skywalker
+   robot.walk(index)
+   robot.calculate_distance(5)
+   robot.calculate_distance(1)
+ end
