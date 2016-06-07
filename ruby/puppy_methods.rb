e4467ab7c2f10e6a2ec43c20dc4980dc85d65817
Milan Cook <milancook@gmail.com>
2016-06-07 18:04:25 -0500

    Added human years method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -8,6 +8,14 @@ class Puppy
 	def speak(number_of_times)
 		number_of_times.times{puts "Woof!"}
 	end
+	
+	def roll_over
+		puts "*roll over*"
+	end
+
+	def dog_years(human_years)
+		human_years*7
+	end
 
 end
 
@@ -16,4 +24,8 @@ puppy = Puppy.new
 puppy.fetch "ball"
 
 puppy.speak 3
+
+puppy.roll_over
+
+puts puppy.dog_years 9
 
