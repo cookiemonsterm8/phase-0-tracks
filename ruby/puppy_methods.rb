0b9db1980b8da861902721497165823d08efae18
Milan Cook <milancook@gmail.com>
2016-06-07 17:54:35 -0500

    added woof method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,9 +4,16 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+	
+	def speak(number_of_times)
+		number_of_times.times{puts "Woof!"}
+	end
 
 end
 
 puppy = Puppy.new
 
 puppy.fetch "ball"
+
+puppy.speak 3
+
