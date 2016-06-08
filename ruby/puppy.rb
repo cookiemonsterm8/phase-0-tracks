8eb67c808464875667b56cdbaddb6c6a6f4602fc
Milan Cook <milancook@gmail.com>
2016-06-07 21:51:48 -0500

    did work from release 0 of puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+#PUPPY
+
+#SPECIES: Canis lupus familiaris
+
+#BREED: Yorkshire Terrier 
+#NAME: Thomas
+#Characteristics
+#Friendly: Extremely
+#Eye Count: 2
+#Color: Brown, black and grey
+#Legs: 4
+
+#BEHAVIOR
+#whining
+#nervousness
+#excitement
+#constant hunger
+#lazy
+
