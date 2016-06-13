930996ff73ce445cedde4f3847090ef1e06820ae
Milan Cook <milancook@gmail.com>
2016-06-13 09:29:55 -0500

    Forgot to checkout branch, adding work for 6.3 and 6.6

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,6 +1,24 @@
 class TodoList
-	def get_list
-		my_list =["do the dishes","mow the lawn"]
-	end
+
+  def initialize(array)
+    @list = array
+  end
+
+  def get_items
+    @list
+  end
+
+  def add_item(item)
+    @list << item
+  end
+
+  def delete_item(item)
+    @list.delete(item)
+  end
+
+  def get_item(item_index)
+    @list[item_index]
+    #or @list.at(item_index)
+  end
 
 end
--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,11 +1,12 @@
-require_relative 'todoList'
+require_relative 'TodoList'
 
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
   it "stores the list items given on initialization" do
-    expect(list.get_items).to eq ["do the dishes" , "mow the lawn"]
+    expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
   end
+
 
   it "adds an item to the list" do
     list.add_item("mop")
@@ -18,6 +19,6 @@ describe TodoList do
   end
 
   it "retrieves an item by index" do
-    expect(list.get_item(0)).to eq "do the dishes"
+   expect(list.get_item(0)).to eq "do the dishes"
   end
 end
